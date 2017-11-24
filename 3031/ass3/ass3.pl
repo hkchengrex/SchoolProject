cd84ab062c8fbb2355955c77ddeb45ec0e676a2f
Rex Cheng <hkchengad@connect.ust.hk>
2017-11-25 01:11:07 +0800

    Q2 done, fucking me up

--- 3031/ass3/ass3.pl
+++ 3031/ass3/ass3.pl
@@ -13,6 +13,9 @@ pro_list(List) :- findall(X, teach(X, _), List).
 member(X, [X|_]).
 member(X, [_|Tail]) :- member(X, Tail).
 
+%% member_sp(X, [X|_]) :- !.
+%% member_sp(X, [_|Tail]) :- member_sp(X, Tail), !.
+
 head(X, [X|_]).
 tail(X, [_|X]).
 
@@ -29,7 +32,23 @@ dis_list_prof_ids(_, [], []) :- !.
 % I did not read the question well and I made a distinct version... It works, so I'll just keep it here
 distinct_prof_ids(Stu, List) :- enroll(Stu, CouList), pro_list(ProList), dis_list_prof_ids(CouList, List, ProList).
 
+% Question 1
 list_prof_ids([CouHead|CouTail], [ResHead|ResTail]) :- teach_course(ResHead, CouHead), list_prof_ids(CouTail, ResTail), !.
 list_prof_ids([], []) :- !.
 
 prof_ids(Stu, List) :- enroll(Stu, CouList), list_prof_ids(CouList, List).
+
+% Question 2
+cou_list_com([Head|ATail], [Head|BTail], [Head|CouTail]) :- cou_list_com(ATail, BTail, CouTail), !.
+cou_list_com([_|ATail], BList, [CouHead|CouTail]) :- cou_list_com(ATail, BList, [CouHead|CouTail]), !.
+cou_list_com(AList, [_|BTail], [CouHead|CouTail]) :- cou_list_com(AList, BTail, [CouHead|CouTail]), !.
+cou_list_com(_, _, []) :- !.
+
+%% common_enroll(StuA, StuB, List) :- StuA \= StuB, enroll(StuA, CouA), enroll(StuB, CouB), cou_list_com(CouA, CouB, List).
+common_enroll(StuA, StuB, List) :- var(StuA), var(List), enroll(StuA, CouA), enroll(StuB, CouB), cou_list_com(CouA, CouB, List), StuA \== StuB.
+common_enroll(StuA, StuB, List) :- var(StuB), var(List), enroll(StuA, CouA), enroll(StuB, CouB), cou_list_com(CouA, CouB, List), StuA \== StuB.
+common_enroll(StuA, StuB, List) :- \+ var(StuA), \+ var(StuB), var(List), enroll(StuA, CouA), enroll(StuB, CouB), cou_list_com(CouA, CouB, List), StuA \== StuB, !.
+common_enroll(StuA, StuB, List) :- \+ var(StuA), \+ var(StuB), \+ var(List), enroll(StuA, CouA), enroll(StuB, CouB),
+	permutation(List, PerRes), cou_list_com(CouA, CouB, PerRes), StuA \== StuB, !.
+
+
